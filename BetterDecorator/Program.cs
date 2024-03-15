1af7c45e7cad7953a3de487fe613d6a01b29e062
HMVMARTINS <hugomvmartins90@gmail.com>
2024-03-15 01:19:01 +0000

    Exercício 3 - Overloading de métodos

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -8,7 +8,7 @@ namespace BetterDecorator
     class Program
     {
         /// <summary>
-        /// Programa principal
+        /// Programa principal.
         /// </summary>
         /// <param name="args">Os argumentos da linha de comandos.</param>
         static void Main(string[] args)
@@ -17,28 +17,39 @@ namespace BetterDecorator
             //Verifica se os argumentos foram passados
             if (args.Length < 3)
             {
-                Console.WriteLine("Usage: dotnet run --project BetterDecorator -- \"<strings>\" \"<char>\" <int>");
-                return;
+                //Se não foram passados, invoca o método Decor()
+                Console.WriteLine(Decor());
             }
+            else
+            {
+                // Extrai os argumentos da linha de comando.
+                string s = args[0]; //A string pode conter espaços.
+                char dec = args[1][0]; //O caracter é o primeiro da segunda string.
+                int repeatCount = int.Parse(args[2]); // Converta a 3ª string para um número inteiro.
 
-            // Extrai os argumentos da linha de comando
-            string s = args[0]; //A string pode conter espaços
-            char dec = args[1][0]; //O caracter é o primeiro da segunda string
-            int repeatCount = int.Parse(args[2]); // Converta a 3ª string para um número inteiro
-
-            // Imprime o resultado aplicando o método Decor
-            Console.WriteLine(Decor(s, dec, repeatCount));
+                // Imprime o resultado aplicando o método Decor.
+                Console.WriteLine(Decor(s, dec, repeatCount));
+            }
         }
 
         /// <summary>
         /// Decora uma string
         /// </summary>
-        /// <param name="s">A string é decorada.</param>
-        /// <param name="dec">É usado um caracter.</param>
-        /// <param name="repeatCount">Número de repetições.</param>
         /// <returns>String decorada.</returns>
-        private static string Decor(string s, char dec, int repeatCount)
+        private static string Decor()
         {
+            return Decor("User did not specify args!", '=', 3);
+        }
+
+        /// <summary>
+        /// String Personalizada
+        /// </summary>
+        /// <param name="s">A string a ser decorada.</param>
+        /// <param name="dec"> Caracter usado.</param>
+        /// <param name="repeatCount">Número de repetições.</param>
+        /// <returns>A string decorada.</returns>
+        private static string Decor(string s, char dec, int repeatCount)
+        {    
             // Constrói o decorador
             string decorator = new string(dec, repeatCount);
             // Constrói a string
