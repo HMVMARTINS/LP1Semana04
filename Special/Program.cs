a81a5522d65398961f179c63e8797efd75df2e15
HMVMARTINS <hugomvmartins90@gmail.com>
2024-03-15 01:42:09 +0000

    Exercício 4 - Sequência Especial

---
+++ Special/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Special
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert n-th number of the special sequence:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetSpecial(n));
+        }
+
+        private static int GetSpecial(int n)
+        { 
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+
+            return 2 * GetSpecial(n - 1) + GetSpecial(n - 2);
+        }
+    }
+}
